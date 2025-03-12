9c43396196a1c413f0ae50a5a69fecebe1b95962
guanxh <337507950@qq.com>
2025-03-13 00:44:29 +0800

    first time to adjust

---
+++ springboot-mybatis/src/main/java/org/spring/springboot/controller/MovieContoller.java
@@ -0,0 +1,53 @@
+package org.spring.springboot.controller;
+
+import com.gavin.movie.domain.Movie;
+import com.gavin.movie.service.IMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieContoller {
+    @Autowired
+    private IMovieService movieService;
+
+    @GetMapping("/allMovies")
+   public List<Movie> getAllMovie() {
+        return movieService.selectAllMovie();
+    }
+
+    @PostMapping("/selectMovieList")
+    public List<Movie> selectMovieList(@RequestBody Movie movie) {
+        System.out.println("movie" + movie);
+        List<Movie> movieList;
+        movieList = movieService.selectMovieList(movie);
+
+        return movieList;
+    }
+
+    @PostMapping("/getMovieByName")
+    public List<Movie> getMovieByName(@RequestBody Movie movie) {
+        System.out.println("movie" + movie);
+        return movieService.selectByName(movie);
+    }
+
+    @PostMapping("/generateMovie")
+    public Integer generateMovie(@RequestBody Movie movie) {
+        System.out.println("movie" + movie);
+        return movieService.insertMovie(movie);
+    }
+
+    @PostMapping("/updateMovie")
+    public Integer updateMovie(@RequestBody Movie movie) {
+        System.out.println("movie" + movie);
+        return movieService.updateMovie(movie);
+    }
+
+    @PostMapping("/deleteMovieByIds/{movieIds}")
+    public int delMovieByIds(@PathVariable Long[] ids) {
+        return movieService.deleteMovieByIds(ids);
+    }
+
+}
