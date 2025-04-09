4771636928ada0f629123c132750b11e140fa5b7
guanxh <337507950@qq.com>
2025-04-09 16:46:37 +0800

    dynamic sql

---
+++ springboot-dynamicsql/src/main/java/org/spring/springboot/enums/DataSourceType.java
@@ -0,0 +1,18 @@
+package org.spring.springboot.enums;
+
+/**
+ * 数据源
+ *
+ * @author colaclub
+ */
+public enum DataSourceType {
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
