5e70fa48e627bd6215cc92aed1fb7c25fbcc9e68
guanxh <337507950@qq.com>
2025-04-14 13:36:11 +0800

    refactor

---
+++ springboot-rbac/src/main/java/org/spring/springboot/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package org.spring.springboot.repository;
+
+import org.spring.springboot.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+
+}
