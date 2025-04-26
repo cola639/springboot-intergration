fe801b3d2f61e8f5d9499cb442f1cedc6c81657f
guanxh <337507950@qq.com>
2025-04-27 03:41:24 +0800

    feat: create movies table and update application configuration

--- springboot-jpa/src/main/java/org/spring/springboot/controller/MovieContoller.java
+++ springboot-jpa/src/main/java/org/spring/springboot/controller/MovieContoller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/movie")
 public class MovieContoller {
 
     @Autowired
--- springboot-jpa/src/main/java/org/spring/springboot/domain/MovieTypes.java
+++
@@ -1,4 +0,0 @@
-package org.spring.springboot.domain;
-
-public class MovieTypes {
-}
