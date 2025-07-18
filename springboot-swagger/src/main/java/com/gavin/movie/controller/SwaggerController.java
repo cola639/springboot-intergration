3e817e47093d7df7dcdfab9d99171c09e991a5d1
cola639 <337507950@qq.com>
2025-07-18 23:26:53 +0800

    refactor: rename MovieApplication to Application and improve code formatting in SwaggerController

--- springboot-swagger/src/main/java/com/gavin/movie/controller/SwaggerController.java
+++ springboot-swagger/src/main/java/com/gavin/movie/controller/SwaggerController.java
@@ -1,6 +1,5 @@
 package com.gavin.movie.controller;
 
-
 import com.gavin.movie.model.SwaggerApiModel;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
+
 /**
  * @author: zjl
  * @datetime: 2024/3/26
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.*;
 public class SwaggerController {
 
     @Operation(summary = "测试Swagger3注解方法Get")
-    @Parameters({@Parameter(name = "id",description = "编码"),
-            @Parameter(name = "headerValue",description = "header传送内容")})
+    @Parameters({@Parameter(name = "id", description = "编码"),
+            @Parameter(name = "headerValue", description = "header传送内容")})
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "请求成功"),
             @ApiResponse(responseCode = "400", description = "请求参数没填好"),
@@ -30,8 +30,8 @@ public class SwaggerController {
             @ApiResponse(responseCode = "404", description = "请求路径没有或页面跳转路径不对")
     })
     @GetMapping(value = "/swagger/student")
-    public Object getStudent(@RequestParam @Parameter(example = "2")  String id,
-                             @RequestHeader @Parameter(example = "2") String headerValue){
+    public Object getStudent(@RequestParam @Parameter(example = "2") String id,
+                             @RequestHeader @Parameter(example = "2") String headerValue) {
         return id;
     }
 
@@ -44,28 +44,29 @@ public class SwaggerController {
             @ApiResponse(responseCode = "404", description = "请求路径没有或页面跳转路径不对")
     })
     @PostMapping(value = "/swagger/student", produces = "application/json")
-    public SwaggerApiModel updateStudent(@RequestBody SwaggerApiModel model){
+    public SwaggerApiModel updateStudent(@RequestBody SwaggerApiModel model) {
         return model;
     }
-
 
     /**
      * swagger 不暴漏该 api，通过@Hidden隐藏
      * 但是仍然可以访问
+     *
      * @return
      */
     @Hidden
     @GetMapping(value = "/swagger/hiddenApi")
-    public String hiddenApi(){
+    public String hiddenApi() {
         return "hiddenApi";
     }
 
     /**
      * swagger 暴漏该 api，没有配置@Hidden会展示
+     *
      * @return
      */
     @GetMapping(value = "/swagger/noHiddenApi")
-    public String noHiddenApi(){
+    public String noHiddenApi() {
         return "noHiddenApi";
     }
 }
